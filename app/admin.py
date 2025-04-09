e9a1daef001e6969c4ca5da935952493bc68fa77
Robocotik <alexander.starkin@gmail.com>
2025-04-09 13:03:54 +0300

    [feat] migrations: use migrations

--- app/admin.py
+++ app/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 
-from .models import Tag, Answer, Question, AnswerLike, QuestionLike, Profile, Like
+from .models import Tag, Answer, Question, AnswerLike, QuestionLike, Profile, Like, QuestionManager
 admin.site.register(Tag)
 admin.site.register(Answer)
-admin.site.register(AnswerLike)
 admin.site.register(Question)
-admin.site.register(QuestionLike)
 admin.site.register(Profile)
 admin.site.register(Like)
+# admin.site.register(AnswerLike)
+# admin.site.register(QuestionLike)
+# admin.site.register(QuestionManager)
 
--- app/models.py
+++ app/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import User
 from django.db.models import Manager
 class Profile(models.Model):
     id = models.BigAutoField(auto_created=True, primary_key=True, serialize=True)
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.PROTECT)
     avatar = models.ImageField(upload_to='avatars/', null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.user
     
 class QuestionManager(Manager):
     def new_questions(self):
@@ -16,34 +19,41 @@ class QuestionManager(Manager):
     
 class Question(models.Model):
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
     tags = models.ManyToManyField('Tag', related_name='questions')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self) -> str:
+        return self.title
+
 class Tag(models.Model):
     title = models.CharField(max_length=255)
-    hh = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.title
     
 class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.PROTECT, related_name='answers', blank=True, null=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT, blank=True, null=True)
     content = models.CharField(max_length=1000)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.question
     
 class QuestionLike(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='likes')
-    like = models.ForeignKey('Like', on_delete=models.CASCADE)
+    question = models.ForeignKey('Question', on_delete=models.PROTECT, related_name='likes', blank=True, null=True)
+    like = models.ForeignKey('Like', on_delete=models.PROTECT, blank=True, null=True)
     class Meta:
         unique_together = ('like', 'question')  # Исправлено на 'like' и 'question'
 class AnswerLike(models.Model):
-    answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='likes')
-    like = models.ForeignKey('Like', on_delete=models.CASCADE)
+    answer = models.ForeignKey('Answer', on_delete=models.PROTECT, related_name='likes', blank=True, null=True)
+    like = models.ForeignKey('Like', on_delete=models.PROTECT, blank=True, null=True)
     class Meta:
         unique_together = ('answer', 'like')  # Исправлено на 'answer' и 'like'
 
 
 class Like(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE) 
-    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='like') 
+    count = models.BigIntegerField(default=0)
