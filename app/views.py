932f5fa99c454e6bc0c6c49c4f07cfbd85c31a15
Robocotik <alexander.starkin@gmail.com>
2025-04-12 17:49:10 +0300

    [feat] errors: check value of pagination

--- app/views.py
+++ app/views.py
@@ -1,15 +1,29 @@
 from django.shortcuts import render
 from static.mock.question import questions
 from .models import Question
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage
 
 
 # Create your views here.
+
+def paginate(objects_list, request, per_page=5):
+    paginator = Paginator(objects_list, per_page)
+    page_num = request.GET.get('page', 1)
+    try:
+        page_num = int(page_num) if int(page_num) > 0 else 1
+    except (ValueError, TypeError):
+        page_num = 1
+    try:
+        page = paginator.page(page_num)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+    
+    return page
+
 def index(request):
-    page_num = int(request.GET.get('page', 1))
+    
     questions = Question.objects.all().prefetch_related('tags')
-    paginator = Paginator(questions, 5)
-    page = paginator.page(page_num)
+    page = paginate(questions, request=request)
     return render(request, 'index.html', context={"items" : page.object_list, 'page_obj': page})
 
 def settings(request):
@@ -22,10 +36,8 @@ def signup(request):
     return render(request, 'register.html')
 
 def hot(request):
-    page_num = int(request.GET.get('page', 1))
     questions = Question.objects.new_questions()
-    paginator = Paginator(questions, 5)
-    page = paginator.page(page_num)
+    page = paginate(questions, request)
     return render(request, 'index.html', context={"items": page, 'page_obj': page})
 
 def ask(request):
@@ -35,10 +47,8 @@ def single_question(request, question_id):
     return render(request, 'single_question.html', context={"item": questions[question_id]})
 
 def tag_id(request, tag):
-    page_num = int(request.GET.get('page', 1))
     questions = Question.objects.questions_with_tag(tag)
-    paginator = Paginator(questions, 5)
-    page = paginator.page(page_num)
+    page = paginate(questions, request)
     return render(request, 'single_tag.html', context={"items": page, 'page_obj': page, "tag": tag})
 
 def custom_404_view(request, exception):
