18598878c24838fbd6ab762ce605c32ead2263ec
Robocotik <alexander.starkin@gmail.com>
2025-04-12 15:31:40 +0300

    [feat] pagintation: add pagination

--- app/views.py
+++ app/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
 from static.mock.question import questions
 from .models import Question
+from django.core.paginator import Paginator
+
+
 # Create your views here.
 def index(request):
+    page_num = int(request.GET.get('page', 1))
     questions = Question.objects.all().prefetch_related('tags')
-    return render(request, 'index.html', context={"items" : questions})
+    paginator = Paginator(questions, 5)
+    page = paginator.page(page_num)
+    return render(request, 'index.html', context={"items" : page.object_list, 'page_obj': page})
 
 def settings(request):
     return render(request, 'settings.html',)
